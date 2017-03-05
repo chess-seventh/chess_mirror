118e671cdd11cea53145b2cf129e905344bbd953
nevr0sed <piva.francesco@gmail.com>
2017-03-05 22:04:18 +0100

    updated config.py

--- backend/constants.py
+++ backend/constants.py
@@ -1,10 +1,10 @@
 
 
 METEO_BASE_URL = 'http://api.openweathermap.org/data/2.5/weather?id='
-METEO_USER_API = '62f54b1ac3be58fdcad32cddd6b0713f'
+METEO_USER_API = ''
 METEO_CITY_IDS = '6458783'
 METEO_UNIT = 'metric'
 
-SMS_USER_API = 'gU0UNSLgwOj69HZOmQ0XSuUKyxNc2CZU'
+SMS_USER_API = ''
 SMS_BASE_URL = 'https://api.swisscom.com/messaging/sms/'
-SMS_SENDER_NUM = '+41798300270'
+SMS_SENDER_NUM = ''
